485ffc6cb97ce97adf576be568a41a4f2346855b
naiknetra <naiknetra5@gmail.com>
2025-01-09 11:50:40 -0500

    updated tsx files

---
+++ Develop/src/pages/CandidateSearch.tsx
@@ -0,0 +1,8 @@
+//import { useState, useEffect } from 'react';
+//import { searchGithub, searchGithubUser } from '../api/API';
+
+const CandidateSearch = () => {
+  return <h1>Candidate Search</h1>;
+};
+
+export default CandidateSearch;
