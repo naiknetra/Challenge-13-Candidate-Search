2e7e2f89b9d0171c1c9b8d7946c2d45447001f33
naiknetra <naiknetra5@gmail.com>
2025-01-15 13:57:28 -0500

    added README

---
+++ Develop/src/components/Candidate.tsx
@@ -0,0 +1,13 @@
+const Candidate = (props) => {
+
+    return (
+    <div>
+    <h1>Candidate</h1>
+<p>Hello {props.user}</p>
+</div>
+);
+
+
+}
+
+export default Candidate;
--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,8 +1,26 @@
-//import { useState, useEffect } from 'react';
-//import { searchGithub, searchGithubUser } from '../api/API';
+import { useEffect } from 'react';
+import { searchGithub } from '../api/API';
+import  Candidate  from '../components/Candidate';
 
 const CandidateSearch = () => {
-  return <h1>Candidate Search</h1>;
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const data = await searchGithub();
+      console.log("test1")
+      console.log(data);
+
+    }
+    fetchdata();
+  })
+  const testUser = "John";
+  return (
+    <div>
+  <h1>Candidate Search</h1>
+  <Candidate user={testUser}/>
+  </div>
+  )
+
 };
 
 export default CandidateSearch;
